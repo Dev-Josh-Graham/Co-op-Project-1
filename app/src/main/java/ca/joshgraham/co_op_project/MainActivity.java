fe7eee25c1f39f23002d3641a8d440f4469f426b
Dev-Josh-Graham <josh8.graham@gmail.com>
2023-09-15 09:28:18 -0500

    Initial Empty Project

---
+++ app/src/main/java/ca/joshgraham/co_op_project/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.joshgraham.co_op_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
